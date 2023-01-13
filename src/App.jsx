d4bae246692c2da09b2c6e89158ffa5e5f10436a
ChezyName <mineboy2468@gmail.com>
2023-01-12 20:48:34 -0600

    Added Bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Button from 'react-bootstrap/Button';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Button></Button>
     </div>
   )
 }
