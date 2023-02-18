a6f756cd661709e587bf3d42da4c4c1a0d3424b7
ChezyName <mineboy2468@gmail.com>
2023-02-18 10:44:24 -0600

    Added User Input

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,22 @@ import TeamSelection from './components/TeamSelection';
 import ButtonList from './components/ButtonList';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [choosingTeam,setChooseTeam] = useState(true);
+  const [TeamNumber,setTeamNumber] = useState(0);
+  const [MatchNumber,setMatchNumber] = useState(0);
+
+  function onTeamChoose(Team,Match){
+    console.log("Got Team @ " + Team + " : " + Match);
+    setTeamNumber(Team);
+    setMatchNumber(Match);
+    setChooseTeam(false);
+  }
 
   return (
     <div style={{width: "100%", height: "100%", backgroundColor: "#101010"}} className="App">
-      <TeamSelection/>
+      {
+        choosingTeam ? <TeamSelection CallbackFunction={onTeamChoose}/> : ""
+      }
       
       <ButtonList Title="CONES Picked Up"/>
       <ButtonList Title="CUBES Picked Up"/>
--- src/components/TeamSelection.jsx
+++ src/components/TeamSelection.jsx
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, {useEffect,useState} from 'react'
 import styles from "./TeamSelection.module.css"
 
-const TeamSelection = () => {
+const TeamSelection = ({CallbackFunction}) => {
+  const params = new URLSearchParams(window.location.search);
+  const team = params.get("team");
+  const match = params.get("match");
+
+  const [teamNum,setTeam] = useState(0); 
+  const [matchNum,setMatch] = useState(0); 
+
+  useEffect(() => {
+    setTeam(team);
+    setMatch(match);
+  },[]);
+  
+
   return (
     <div style={{width: '100%', height: '100%', backgroundColor: 'rgba(50,50,50,0.8)', position: 'absolute', zIndex: '99999 !importiant'}}>
         <div className={styles.z} style={{width: '80%', height: '80%', backgroundColor: 'rgb(32, 32, 32)',position: 'absolute', top: '10%', left: '10%'}}>
 
             <div style={{width: '100%',height: '8%',color: 'white',fontSize: '4vh', borderBottom: '1px solid white', display: 'block'}}>Match Number:</div>
-            <input type='number' style={{width: '20%', height: '10%', marginTop: '1%', marginBottom: "3%", display: 'block'}}></input>
+            <input value={teamNum} onChange={(event) => {setTeam(event.target.value)}} type='number' style={{width: '20%', height: '10%', marginTop: '1%', marginBottom: "3%", display: 'block'}}></input>
 
             <div style={{width: '100%',height: '8%',color: 'white',fontSize: '4vh', borderBottom: '1px solid white', display: 'block'}}>Team Number:</div>
+            <input value={matchNum} onChange={(event) => {setMatch(event.target.value)}}  type='number' style={{width: '20%', height: '10%', marginTop: '1%', marginBottom: "3%", display: 'block'}}></input>
+            {/*
             <select style={{width: '80%', height: '8%', marginLeft: '10%', marginTop: '1%'}}>
               <option value="2846">Firebears</option>
               <option value="1010">Binary</option>
               <option value="0000">N/A</option>
             </select>
+            */}
+            
+            <button
+            style={{
+              width: '100%',
+              height: 'auto',
+              position: 'absolute',
+              bottom: '0%',
+            }}
+            onClick={
+              () => {
+                console.log("Chose Team @ " + teamNum + " : on Match " + matchNum);
+                CallbackFunction(teamNum,matchNum)
+              }
+            }
+            >CONFIRM</button>
         </div>
     </div>
   )
